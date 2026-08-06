22f02c199d5ab561c5c79ddeab99e845af26bec1
samarpro <samarpangupta123@gmail.com>
2026-08-06 11:26:33 +1000

    added: data models

--- src/models/menu.ts
+++ src/models/menu.ts
@@ -1,7 +1,7 @@
 import type { Product } from "./product.js";
 
 export interface Menu {
-  readonly id: string;
+  readonly id: string; // handy when introduced multiple menus in the future
   readonly name: string;
   readonly products: readonly Product[];
 }
